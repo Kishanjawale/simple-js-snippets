9604852f4bb01a6486c2d2e09bb095d985f7ab04
KISHAN JAWALE <kishanjawale10@gmail.com>
2024-01-19 11:35:03 +0530

    Add files via upload

---
+++ 04_Premitive.js
@@ -0,0 +1,25 @@
+//There are 7 type of premitive data types in Javascript
+    //NULL
+    //NUMBER
+    //BOOLEAN
+    //BIG-INT
+    //SYMBOL
+    //STRING
+    //UNDEFINED
+
+let a = null                         //null      
+let b = 5634;                         //number
+let c =true;                          //boolean
+let d =BigInt(7623);                  //big int
+let e ="This is a String"             //string
+let f =Symbol("This is a symbol");    //symbol
+let g = undefined;                    //undefined
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(typeof a);
